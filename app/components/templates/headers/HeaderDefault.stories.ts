b48117dad53d9b758b2576259262d3b8605f042c
D-Nancy <42789720+D-Nancy@users.noreply.github.com>
2020-03-25 00:29:02 +0900

    fix HeaderDefaultのStorybookの修正完了

--- app/components/templates/headers/HeaderDefault.stories.ts
+++ app/components/templates/headers/HeaderDefault.stories.ts
@@ -1,15 +1,15 @@
 import HeaderDefault from './HeaderDefault.vue'
 
 // @ts-ignore
-import mdx from './HeaderDefault.mdx'
+// import mdx from './HeaderDefault.mdx'
 
 export default {
   title: 'templates/headers/HeaderDefault',
-  parameters: {
-    docs: {
-      page: mdx,
-    },
-  },
+  // parameters: {
+  //   docs: {
+  //     page: mdx,
+  //   },
+  // },
   component: HeaderDefault,
 }
 
