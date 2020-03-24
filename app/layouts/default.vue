21b2dffb724eb82e3a074343e716ea0dabdf4a63
alizlab-nancy <52145579+alizlab-nancy@users.noreply.github.com>
2020-03-24 16:26:35 +0900

    Initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="layout layout-default">
+    <main class="content">
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LayoutDefault',
+})
+</script>
+
+<style lang="scss" scoped>
+  .layout-default {
+    position: relative;
+    min-height: 100vh;
+    padding: 80px 0;
+
+    > .header {
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 7000;
+    }
+
+    > .content {
+    }
+
+    > .footer {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+    }
+  }
+</style>
